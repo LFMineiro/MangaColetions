f5c2ca538694945da650984c4d63e1e9fc3e97b7
LFMineiro <lflmalbuquerque3@gmail.com>
2025-04-16 01:02:53 -0300

    Add set, get and output requisited

--- main.cpp
+++ main.cpp
@@ -9,19 +9,74 @@ class Manga {
         int num_pag;
     public:
         Manga(string tit="", string aut = "", int pag = 0) {
-        titulo = tit;
-        autor = aut;
-        num_pag = pag;
+            titulo = tit;
+            autor = aut;
+            num_pag = pag;
         }
 
-        bool mangaLongo(int numPag) {
-            if(numPag > 500) return true;
-            else false;
+        bool mangaLongo() {
+             return num_pag > 500;
          }
 
+         // Set
+        void setTituloManga(string &tManga) {
+            if(titulo != "")
+                titulo = tManga;
+            else
+                cout << "o titulo nao podem estar em branco";
+        } 
+        void setAutorManga(string aManga) {
+            if(autor != "") 
+                autor = aManga;
+            else
+                cout << "o autor nao podem estar em branco";
+        } 
+        void setPagManga(int pManga) {
+           if(pManga > 0) 
+                num_pag = pManga;
+            else 
+                cout << "O numero de paginas tem que ser um numero positivo" ;   
+        } 
+
+         // Get
+        string getTituloManga() {
+           return titulo;       
+        } 
+        string getAutorManga() {
+          return  autor;        
+        } 
+        int getPagManga() {
+          return num_pag; 
+        }
 };
 
 int main() {
+    string title;
+    string autor;
+    int numPag = 0;
+    
    Manga m1 = Manga();
    
+    cout << "Qual o titulo do manga: " ;
+    cin >> title;
+
+   cout << "Qual o autor do manga: ";
+   cin >> autor;
+
+while(true) {
+   cout << "Quantas páginas tem: ";
+   cin >> numPag;
+
+   if(numPag > 0) {
+    m1.setPagManga(numPag);
+    break;
+   }
+   cout << "Por favor, insira um número de páginas válido.\n";
+}
+
+   m1.setAutorManga(autor);
+   m1.setTituloManga(title);
+   m1.setPagManga(numPag);
+
+   cout << "O mangá " << m1.getTituloManga() << " de "  << m1.getAutorManga() << " possui " << m1.getPagManga() << " páginas, sendo " << (m1.mangaLongo() ? "Longo" : "NAO LONGO");
 }
